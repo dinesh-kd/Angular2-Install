eb917e19192afe9696a9ed51c8a0f8073847efe4
Angular CLI <angular-cli@angular.io>
2017-03-07 13:25:11 +0530

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Angular2InstallPage } from './app.po';
+
+describe('angular2-install App', () => {
+  let page: Angular2InstallPage;
+
+  beforeEach(() => {
+    page = new Angular2InstallPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
